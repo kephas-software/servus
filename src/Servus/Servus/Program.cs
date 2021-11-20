eed2b643b8ba3aa916bc07c05a17c3c03d9d1e46
Ioan Crișan <ioan.crisan@kephas-software.com>
2021-11-20 14:28:40 +0200

    Iteration 1: Quick and dirty.

---
+++ src/Servus/Servus/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Servus, how can I help you?");
+Console.WriteLine("Type 'help' for the list of available commands, and 'quit' to exit the application. Have fun!");
+
+Console.Write("> ");
+var command = Console.ReadLine();
+
+while (command != "quit")
+{
+    switch (command)
+    {
+        case "help":
+            Console.WriteLine("quit: exits the application.");
+            Console.WriteLine("help: displays the list of available commands.");
+            Console.WriteLine("order <item>: orders the provided item.");
+            Console.WriteLine("find <something or someone>: finds something or someone.");
+            break;
+        case string orderCommand when orderCommand.StartsWith("order "):
+            Console.WriteLine($"Your order for {orderCommand[6..]} will be delivered soon!");
+            break;
+        case string findCommand when findCommand.StartsWith("find "):
+            Console.WriteLine($"Trying to find {findCommand[5..]}, but implementation is missing :(.");
+            break;
+        default:
+            Console.WriteLine($"Sorry, I don't know how to handle your '{command}' command. Try 'help' for the list of available commands.");
+            break;
+    }
+
+    Console.Write("> ");
+    command = Console.ReadLine();
+}
+
+Console.WriteLine("Anytime at your service. See you soon!");
---
+++ src/Servus/Servus/Servus.md
@@ -0,0 +1,11 @@
+﻿# The Servus application
+
+## Iteration 1
+
+Create a command line application which:
+
+* Reads the input provided by the user, interprets it, and provides an output in the command line.
+* Commands are processed one by one.
+  * ``quit``: exits the application displaying a goodbye message.
+  * ``order``: orders items to buy.
+  * ``find``: finds something or someone.
